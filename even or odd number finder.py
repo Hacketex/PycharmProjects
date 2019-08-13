3a2bee3a1a5fd343cc82bae15e8fb4774460bf6d
hacketex <pratikdhole786@gmail.com>
2019-08-13 10:18:29 +0530

    find which number is even and odd

---
+++ even or odd number finder.py
@@ -0,0 +1,9 @@
+# author=pratik
+# created date=14 jun 2019
+# last modified date=14 jun 2019
+# http://github.com/hacketex
+a = int(input("Enter the number   :"))
+if a % 2 == 0:
+    print("%d is even number " % a)
+else:
+    print("%d is odd number " % a)
