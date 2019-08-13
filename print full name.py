24a302a42eaa35e4282f83a271840392e5da3934
hacketex <pratikdhole786@gmail.com>
2019-08-13 10:02:06 +0530

    first program

---
+++ print full name.py
@@ -0,0 +1,7 @@
+# author=pratik
+# created date=10 jun 2019
+# last modified date=10 jun 2019
+# http://github.com/hacketex
+a = (input('enter first name'))
+b = (input('enter last name'))
+print(a + b)
