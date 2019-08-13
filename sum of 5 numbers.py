75165e45a1899e9c32c96ec5ddd3555fbf5cc423
hacketex <pratikdhole786@gmail.com>
2019-08-13 10:26:18 +0530

    14 sum of 5 numbers

---
+++ sum of 5 numbers.py
@@ -0,0 +1,9 @@
+a = int(input("enter the 1st number  "))
+b = int(input("enter the 2nd number  "))
+c = int(input("enter the 3rd number  "))
+d = int(input("enter the 4th number  "))
+e = int(input("enter the 5th number  "))
+sum1 = a + b + c + d + e
+multiply = a * b * c * d * e
+print("sum of 5 number is %d" % sum1)
+print("multiplication of 5 numbers is %d" % multiply)
