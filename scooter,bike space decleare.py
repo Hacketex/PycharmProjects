824302e927b13b400a4302f53b40a63940831e91
hacketex <pratikdhole786@gmail.com>
2019-08-13 10:16:07 +0530

    space declare billing price in differnt ways

---
+++ scooter,bike space decleare.py
@@ -0,0 +1,19 @@
+# author=pratik
+# created date=12 jun 2019
+# last modified date=12 jun 2019
+# http://github.com/hacketex
+scooter = 2098
+bike = 3002
+sportbike = 5000
+car = 102343
+bicycle = 1232.323400
+petrol = 231.320000
+print("  scooter:\t\t%d  \n  bike:\t\t%d  \n  sportbike:\t%d  \n  car:\t%d  \n  bicycle:\t%f  \n  petrol:\t %f  " % (
+scooter, bike, sportbike, car, bicycle, petrol))
+print()
+print(
+    "\t\t\t\t\tscooter:%32d \n\t\t\t\t\t   bike:%32d \n \t\t\t\t  sportbike:%32d  \n  \t\t\t\t\t\tcar:%34d  \n  \t\t\t\t\tbicycle:%39f  \n  \t\t\t\t\t petrol:%38f  " % (
+    scooter, bike, sportbike, car, bicycle, petrol))
+print()
+print("  scooter:%32d  \n  bike:%35d  \n  sportbike:%30d  \n  car:%38d  \n  bicycle:%39f  \n  petrol:%35.2f  " % (
+scooter, bike, sportbike, car, bicycle, petrol))
