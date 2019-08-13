de734019d116d839873a62ed85e75b784fdedcef
hacketex <pratikdhole786@gmail.com>
2019-08-13 10:08:52 +0530

    float format print

---
+++ price per ltr.py
@@ -0,0 +1,9 @@
+# author=pratik
+# created date=11 jun 2019
+# last modified date=11 jun 2019
+# http://github.com/hacketex
+head = "price per liter"
+value = 1.2234545
+print("%s :\t%f" % (head, value))
+print("%s : %17.2f" % (head, value))
+print("%s : %017.2f" % (head, value))
