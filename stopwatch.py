20c805c634e1f3480753f5cc44bf698a8b63a03a
hacketex <pratikdhole786@gmail.com>
2019-08-13 19:21:52 +0530

    lap time by number of rounds pending

--- stopwatch.py
+++ stopwatch.py
@@ -1,13 +1,16 @@
 no_of_round = int(input("enter the number of round:  "))
 n = 1
+slow = 0
+fast = 0
 while n <= no_of_round:
     lap = float(input("enter the lap time for %d lap:  " % n))
     slow = fast = lap
     if slow < lap:
         slow = lap
-        # print("slower",slow)
+        print("slower",slow)
     else:
         fast = lap
-        # print("fast",fast)
+        print("fast",fast)
+    slow = slow + lap
     n = n + 1
 print("total round = %d\nslowest lap = %d\nfastest lap =%d" % (n, slow, fast))
