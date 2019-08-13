306e02971e48784fe1afedad8d7647a7c18cb652
hacketex <pratikdhole786@gmail.com>
2019-08-13 10:04:09 +0530

    multiply, add, power, devide 2 numbers

---
+++ calculation of 2 num.py
@@ -0,0 +1,16 @@
+# author=pratik
+# created date=10 jun 2019
+# last modified date=10 jun 2019
+# http://github.com/hacketex
+a = int(input("enter the 1st number  "))
+b = int(input("enter the 2nd number  "))
+sum1 = (a + b)
+div = a / b
+multiply = a * b
+sub = a - b
+power = (a ** 2, b ** 2, a ** 2 + b ** 2)
+print("addition of two numbers is ", sum1)
+print("division of two numbers is ", div)
+print("multiplication of two numbers is ", multiply)
+print("power of two numbers is ", power)
+print("substraction of two numbers is ", sub)
