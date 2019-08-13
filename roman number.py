d77bf04f73340ae46432ae258ba65053644cf152
hacketex <pratikdhole786@gmail.com>
2019-08-13 10:55:40 +0530

    convert number from 1 to 10 to roman numbers

---
+++ roman number.py
@@ -0,0 +1,26 @@
+# author=pratik
+# created date=2 aug 2019
+# https://github.com/hacketex
+num = int(input("enter the number to convert into roman number:  "))
+if num == 1:
+    print(num, ">> I")
+elif num == 2:
+    print(num, ">> II")
+elif num == 3:
+    print(num, ">> III")
+elif num == 4:
+    print(num, ">> IV")
+elif num == 5:
+    print(num, ">> V")
+elif num == 6:
+    print(num, ">> VI")
+elif num == 7:
+    print(num, ">> VII")
+elif num == 8:
+    print(num, ">> VIII")
+elif num == 9:
+    print(num, ">> IX")
+elif num == 10:
+    print(num, ">> X")
+else:
+    print("error 404")
