f9304a1c10e3854ed904c1a91889bd3545979582
hacketex <pratikdhole786@gmail.com>
2019-08-13 10:06:38 +0530

    student subject marks avrage find

---
+++ marksheet avarage.py
@@ -0,0 +1,15 @@
+# author=pratik
+# created date=10 jun 2019
+# last modified date=10 jun 2019
+# http://github.com/hacketex
+firstname = input("enter ur first name")
+lastname = input("enter ur last name")
+print('welcome', firstname, lastname)
+english = int(input("enter ur english subject marks"))
+hindi = int(input("enter ur hindi subject marks"))
+maths = int(input("enter ur math\'s subject marks"))
+chemistry = int(input("enter ur chemistry subject marks"))
+physics = int(input("enter ur physics subject marks"))
+sum1 = english + hindi + physics + chemistry + maths
+div = sum1 / 5
+print(div)
