2f6756d107e1cf2b68c93cb2fec26e3225aac838
hacketex <pratikdhole786@gmail.com>
2019-08-13 10:12:49 +0530

    space declare by item quentity bill

---
+++ item quentity price bill.py
@@ -0,0 +1,16 @@
+# author=pratik
+# created date=11 jun 2019
+# last modified date=11 jun 2019
+# http://github.com/hacketex
+item = "enter the number of item 12"
+quentity = "enter the quentity  23"
+ppu = "enter the price per unit 44.22"
+print("%s\n%s\n%s\n" % (item, quentity, ppu))
+a = "iteam"
+b = "quentity"
+unit = "price per unit"
+total = "total price"
+line = "_______________"
+print("%s\t\t\t%s\t\t\t%s\t\t\t%s" % (a, b, unit, total))
+print(line * 5)
+print("12                   23                  44.22                 1017.6")
