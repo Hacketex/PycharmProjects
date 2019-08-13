cbd15aabdaf73f7c6605df47d14fab7ccf991d45
hacketex <pratikdhole786@gmail.com>
2019-08-13 10:14:09 +0530

    +----+-----+ square print

---
+++ circuit bord +-.py
@@ -0,0 +1,10 @@
+# author=pratik
+# created date=12 jun 2019
+# last modified date=12 jun 2019
+# http://github.com/hacketex
+a = "+----+" * 3
+b = "|    |" * 3
+print("%s\n%s" % (a, b))
+print("%s\n%s" % (a, b))
+print("%s\n%s" % (a, b))
+print("%s" % a)
