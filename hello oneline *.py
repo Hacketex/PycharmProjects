7286d8dd6246018eddef0ac185d04bb3e63902fe
hacketex <pratikdhole786@gmail.com>
2019-08-13 10:17:26 +0530

    print hello by using * in online

---
+++ hello oneline *.py
@@ -0,0 +1,29 @@
+# author=pratik
+# created date=12 jun 2019
+# last modified date=12 jun 2019
+# http://github.com/hacketex
+a = "*"
+b = " "
+c = "*" * 5
+d = "*" * 4
+print("%s%3s%s" % (a, b, a))
+print("%s%3s%s" % (a, b, a))
+print(c)
+print("%s%3s%s" % (a, b, a))
+print("%s%3s%s" % (a, b, a))
+print()
+print("%s\n%s\n%s\n%s\n%s" % (c, a, d, a, c))
+print()
+print(a)
+print(a)
+print(a)
+print(a)
+print(c)
+print()
+print(a)
+print(a)
+print(a)
+print(a)
+print(c)
+print()
+print(" %s%s%s \n%s   %s \n%s   %s  \n%s   %s  \n %s%s%s " % (a, a, a, a, a, a, a, a, a, a, a, a))
