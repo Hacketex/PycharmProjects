7044556d9fa80092af4223de116e8d369709ea96
hacketex <pratikdhole786@gmail.com>
2019-08-13 10:51:24 +0530

    get error message when no not in range

---
+++ error.py
@@ -0,0 +1,8 @@
+# author=pratik
+# created date=2 aug 2019
+# https://github.com/hacketex
+num = int(input("enter the number between 1 to 12 :  "))
+if num < 13:
+    print('your number is in range between 1 to 12')
+else:
+    print('error 404 number not found')
