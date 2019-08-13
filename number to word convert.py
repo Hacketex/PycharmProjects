e30ff95a6f04035ea9e173d1f3574dbc94413197
hacketex <pratikdhole786@gmail.com>
2019-08-13 11:21:14 +0530

    convert no to word like hundred, thousand

---
+++ number to word convert.py
@@ -0,0 +1,44 @@
+num = int(input("Enter the number:  "))
+tens = num % 10
+if num == 1:
+    print('one')
+elif num == 2:
+    print("two")
+elif num == 3:
+    print("three")
+elif num == 4:
+    print("four")
+elif num == 5:
+    print("five")
+elif num == 6:
+    print("six")
+elif num == 7:
+    print("seven")
+elif num == 8:
+    print("eight")
+elif num == 9:
+    print("nine")
+elif tens == 0:
+    print("ten")
+elif tens == 1:
+    print("eleven")
+elif tens == 2:
+    print("twelve")
+elif tens == 3:
+    print("thirteen")
+elif tens == 4:
+    print("fourteen")
+elif tens == 5:
+    print("fifteen")
+elif tens == 6:
+    print("sixteen")
+elif tens == 7:
+    print("seventeen")
+elif tens == 8:
+    print("eighteen")
+elif tens == 9:
+    print("nineteen")
+elif tens == 0:
+    print("twenty")
+else:
+    print("end")
